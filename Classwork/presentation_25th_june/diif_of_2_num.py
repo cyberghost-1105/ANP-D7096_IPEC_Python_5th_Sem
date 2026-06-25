69c221b875b30ca162256e87483ef7880e94f79d
cyberghost-1105 <123jchirag@gmail.com>
2026-06-25 12:26:17 +0530

    diff to 2 numbers

---
+++ Classwork/presentation_25th_june/diif_of_2_num.py
@@ -0,0 +1,17 @@
+'''to calculate the difference of two numbers'''
+
+#inputing two numbers from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("---------------------------------")
+
+#calculating the difference
+print("The difference of", num1, "and", num2, "is:", num1-num2)
+
+
+'''output:
+Enter first number: 10
+Enter second number: 5
+The difference of 10.0 and 5.0 is: 5.0
+---------------------------------
+'''
