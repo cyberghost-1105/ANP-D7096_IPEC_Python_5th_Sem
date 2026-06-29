b84f6a308b4b981d0ac76ab3672503fdf13d385a
cyberghost-1105 <123jchirag@gmail.com>
2026-06-29 11:21:47 +0530

    employee salary analysis

---
+++ Classwork/day_4_iteration_27th_june_29th_june/employee_sal.py
@@ -0,0 +1,11 @@
+''' Employee Salary Statistics 
+Problem Statement 
+A company has N employees. 
+Accept the salary of each employee and determine: 
+• Highest salary  
+• Lowest salary  
+• Average salary  
+• Number of employees earning more than ₹50,000  
+'''
+
+#coding starts here 
